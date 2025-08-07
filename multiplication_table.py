5efefa909d96f3d6362d749b0195ba1d469a75a8
Fabiano Alves <fabianomalves@proton.me>
2025-08-07 04:35:09 -0300

    Updated script multiplication table

--- multiplication_table.py
+++ multiplication_table.py
@@ -2,23 +2,22 @@
 """
 Print the multiplication table from 1 to 10.
 
-multiplication table 1
-1
-2
-3
+---multiplication table 1---
+        1 X 1 = 1
+        2 X 1 = 2
+        3 X 1 = 3
 ...
-__________
+############################
 
 multiplication table 2
-2
-4
-6
+        2 X 1 = 2
+        2 X 2 = 4
+        3 X 2 = 6
 ...
-__________
+############################
 """
-__version__ = "0.0.1"
+__version__ = "0.1.1"
 __author__ = "Fabiano Alves"
-
 
 # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -26,8 +25,10 @@ numbers = list(range(1,11))
 
 # Iterables
 
-for number in numbers:
-    print(f"Multiplication table {number}")
-    for other_number in numbers:
-        print(number * other_number)
-    print ("_________________")
+for number_1 in numbers:
+        print("{:-^18}".format(f"Multiplication Table {number_1}"))
+        print()
+        for number_2 in numbers:
+            result = number_1 * number_2
+            print("{:^18}".format(f"{number_1} X {number_2} = {result}\n"))
+        print("#" * 23)
