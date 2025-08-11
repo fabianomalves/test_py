2b339ea49391b83fe074f44a5e619016cebbeaf6
Fabiano Alves <fabianomalves@proton.me>
2025-08-11 08:18:16 -0300

    Updated script product_registration.py

--- product_registration.py
+++ product_registration.py
@@ -2,24 +2,36 @@
 """
 Product Registration
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Fabiano Alves"
 
-produto_nome = "Caneta"
-produto_cor1 = "azul"
-produto_cor2 = "branco"
-produto_preco = 3.23
-produto_dimensao_altura = 12.1
-produto_dimensao_largura = 0.8
-produto_em_estoque = True
-produto_codigo = 45678
-produto_codebar = None
+from pprint import pprint
 
+product = {
+    "name": "Caneta",
+    "cores": ["azul", "branco"],
+    "preco": 3.23,
+    "dimensao": {
+        "altura": 12.1,
+        "largura": 0.8,
+    },
+    "em_estoque": True,
+    "codigo": 45678,
+    "codebar": None,
+}
 
-compra = ("Bruno", produto_nome, 3)
-total_compra = compra[2] * produto_preco
+client = {"name": "Fabiano"}
 
-print(
-    f"The client {compra[0]}, bought a {compra[1]}"
-    f" and paid the total of {total_compra}"      
-)
+buy = {
+    "client": client,
+    "product": product,
+    "quantity": 3,
+}
+
+# pprint(buy)
+
+total_buy = buy["quantity"] * buy["product"]["preco"]
+
+print(f"The client {buy['client']['name']}"
+      f" bought a {buy['quantity']} units of {buy['product']['name']}"
+      f" and paid the total of {total_buy}")
